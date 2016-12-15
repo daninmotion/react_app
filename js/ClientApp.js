a5813fdc44bad572c8cc20eea4fc5e0547aadd8c
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-12-15 14:41:34 -0500

    Added bundle.js

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,23 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+var MyTitle = require('./MyTitle.js')
 var div = React.DOM.div
-var h1 = React.DOM.h1
 
-var MyTitle = React.createClass({
-	render () {
-		return (
-			div (null,
-				h1({style: {color: this.props.color}}, this.props.title)
-			)
-		)
-	}
-})
 
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
 	div(null,
-		MyTitleFact({title: "Props are great!", color: "rebeccapurple"}),
-		React.createElement(MyTitle, {title: "Use props everywhere!", color: "mediumaquamarine"})
+		MyTitleFact({title: 'Props are great!', color: 'rebeccapurple'}),
+		React.createElement(MyTitle, {title: 'Use props everywhere!', color: 'mediumaquamarine'})
 	)
 )
 
-ReactDOM.render(MyFirstComponent, document.getElementById("app"))
+ReactDOM.render(MyFirstComponent, document.getElementById('app'))

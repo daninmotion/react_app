8172754c1abc7fc90e1f7b66afb45cd7a7aff20a
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-12-17 11:45:06 -0500

    App component

---
+++ js/ClientApp.jsx
@@ -0,0 +1,16 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+var App = function () { 
+	return (
+		<div className = 'app-container'>
+			<div className = 'home-info'>
+				<h1 className = 'title'>svideo</h1>
+				<input className = 'search' type = 'text' placeholder = 'Search' />
+				<button className = 'browse-all'> or Browse All </button>
+			</div>
+		</div>
+	)
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))

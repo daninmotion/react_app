f610a65c71506ed60435c8d8f72b84337623d5c8
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-12-26 11:07:54 -0500

    Added filter function

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,18 +2,32 @@ var React = require ('react')
 var ShowCard = require('./ShowCard.jsx')
 var data = require ('../public/data.json')
 
-var Search = function () {
-	return (
-		<div className = 'container'>
-			<div className = 'shows'>
-				{data.shows.map(function (show) {
-					return (
-						<ShowCard {...show} key={show.imdbID} />
-					)
-				})}
+var Search = React.createClass({
+	getInitialState () {
+		return {
+			searchTerm: ''
+		}
+	},
+	handleSearchTermEvent (event) {
+		this.setState({searchTerm: event.target.value})
+	},
+	render () {
+		return (
+			<div className = 'container'>
+				<header className = 'header'>
+					<h1 className = 'brand'>svideo</h1>
+					<input value = {this.state.searchTerm} className = 'search-input' type = 'text' placeholder = 'Search' onChange = {this.handleSearchTermEvent} />
+				</header>
+				<div className = 'shows'>
+					{data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+          ))}
+				</div>
 			</div>
-		</div>
-	)
-}
+		)
+	}
+})
 
 module.exports = Search 
